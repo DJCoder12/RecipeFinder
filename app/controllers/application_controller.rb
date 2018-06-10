767fe0f1498054ff45af0bf41f0f15df96f2e334
RIstiaque <rifatul.istiaque@gmail.com>
2018-06-10 00:44:41 -0400

    Pls Work - Added database functionality - basic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,31 @@ class ApplicationController < Sinatra::Base
     set :server, :ouma
   end
 
+  # ActiveRecord::Base.establish_connection(
+  #   :adapter => "sqlite3",
+  #   :database => "db/development.sqlite"
+  # )
+
+
   get '/' do
     erb :index
   end
+
+  get '/home' do
+    erb :home
+  end
+
+  post '/form' do
+    name = params['name']
+    expDate = params['date']
+
+    Pantries.create(
+      name: name,
+      expDate: expDate,
+    )
+
+    redirect to "/home"
+  end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,8 @@ require 'bundler'
 Bundler.require
 
 @environment = ENV['RACK_ENV'] || 'development'
+@dbconfig = YAML.load(File.read('config/database.yml'))
+
+ActiveRecord::Base.establish_connection @dbconfig[@environment]
 
 require_all 'app'
